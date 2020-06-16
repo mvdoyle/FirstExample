8c1142f6853597499ef4423fab139c20f9ccc1b8
mvdoyle <mdoyle@truecoders.io>
2020-06-16 10:37:35 -0500

    added a for loop

--- FirstExample/Program.cs
+++ FirstExample/Program.cs
@@ -7,6 +7,11 @@ namespace FirstExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Hello {i}");
+            }
         }
     }
 }
