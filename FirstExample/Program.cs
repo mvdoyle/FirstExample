c586e9dd241d4ef82ea46e761b13bfcfd4312724
mvdoyle <mdoyle@truecoders.io>
2020-06-16 10:32:56 -0500

    created project

---
+++ FirstExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
